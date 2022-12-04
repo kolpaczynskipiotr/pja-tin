43f4243bbe33c8843cdcae992eb1900bc58c9064
Piotr Kołpaczyński <s20539@pjwstk.edu.pl>
2022-12-04 11:30:14 +0100

    chore: update build

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/pja-tin_01/',
+    base: '/pja-tin/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
